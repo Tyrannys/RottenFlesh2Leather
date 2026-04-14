f27b916726b09b0123b93acffbc76326dd0dfc75
Tyrannys <80750971+Tyrannys@users.noreply.github.com>
2026-04-14 01:08:42 -0500

    26.1 Update
    -Updated to 26.1
    -Fixed a lot of code 🐱

--- src/main/java/net/tyrannys/rottenflesh2leather/RottenFlesh2Leather.java
+++ src/main/java/net/tyrannys/rottenflesh2leather/RottenFlesh2Leather.java
@@ -1,96 +1,233 @@
 package net.tyrannys.rottenflesh2leather;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.*;
+import org.bukkit.inventory.BlastingRecipe;
+import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.SmokingRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RottenFlesh2Leather extends JavaPlugin implements Listener {
-    NamespacedKey RottenLeatherFurnaceKey = new NamespacedKey(this, "rottenleatherfurnace");
-    NamespacedKey RottenLeatherCraftingKey = new NamespacedKey(this, "rottenleathercrafting");
-    NamespacedKey RottenLeatherBlastingKey = new NamespacedKey(this, "rottenleatherblasting");
-    NamespacedKey RottenLeatherSmokerKey = new NamespacedKey(this, "rottenleathersmoker");
+    private static final String ROOT_COMMAND = "rottenflesh2leather";
+    private static final String RELOAD_SUBCOMMAND = "reload";
+    private static final String RELOAD_PERMISSION = "rottenflesh2leather.reload";
+    private static final int BSTATS_PLUGIN_ID = 23949;
 
-    public static boolean furnaceRecipe;
-    public static boolean smokerRecipe;
-    public static boolean blastingRecipe;
-    public static boolean craftingRecipe;
-    public static String craftingRecipeTop;
-    public static String craftingRecipeMid;
-    public static String craftingRecipeBot;
-    public static int cookingTime;
-    public static float exp;
+    private NamespacedKey rottenLeatherFurnaceKey;
+    private NamespacedKey rottenLeatherCraftingKey;
+    private NamespacedKey rottenLeatherBlastingKey;
+    private NamespacedKey rottenLeatherSmokerKey;
+    private PluginSettings loadedSettings;
 
     @Override
     public void onEnable() {
-        ItemStack RottenLeather = new ItemStack(Material.LEATHER, 1);
-        Material RottenFlesh = Material.ROTTEN_FLESH;
-
-        RottenFlesh2Leather.cookingTime = this.getConfig().getInt("cooking-time");
-        RottenFlesh2Leather.exp = this.getConfig().getInt("experience-gain");
-        RottenFlesh2Leather.furnaceRecipe = this.getConfig().getBoolean("furnace-recipe");
-        RottenFlesh2Leather.smokerRecipe = this.getConfig().getBoolean("smoker-recipe");
-        RottenFlesh2Leather.blastingRecipe = this.getConfig().getBoolean("blasting-recipe");
-        RottenFlesh2Leather.craftingRecipe = this.getConfig().getBoolean("crafting-recipe");
-
-        RottenFlesh2Leather.craftingRecipeTop = this.getConfig().getString("crafting-recipe-top");
-        RottenFlesh2Leather.craftingRecipeMid = this.getConfig().getString("crafting-recipe-mid");
-        RottenFlesh2Leather.craftingRecipeBot = this.getConfig().getString("crafting-recipe-bot");
         saveDefaultConfig();
+        initializeBstats();
 
-        FurnaceRecipe rottenLeatherFurnaceRecipe = new FurnaceRecipe(
-                RottenLeatherFurnaceKey,
-                RottenLeather,
-                RottenFlesh,
-                exp,
-                cookingTime);
+        rottenLeatherFurnaceKey = new NamespacedKey(this, "rottenleatherfurnace");
+        rottenLeatherCraftingKey = new NamespacedKey(this, "rottenleathercrafting");
+        rottenLeatherBlastingKey = new NamespacedKey(this, "rottenleatherblasting");
+        rottenLeatherSmokerKey = new NamespacedKey(this, "rottenleathersmoker");
 
-        BlastingRecipe rottenLeatherBlastingRecipe = new BlastingRecipe(
-                RottenLeatherBlastingKey,
-                RottenLeather,
-                RottenFlesh,
-                exp,
-                cookingTime/2
+        loadedSettings = readSettings(getConfig());
+        reconcileRecipes(null, loadedSettings);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!command.getName().equalsIgnoreCase(ROOT_COMMAND)) {
+            return false;
+        }
+
+        if (args.length != 1 || !args[0].equalsIgnoreCase(RELOAD_SUBCOMMAND)) {
+            sender.sendMessage("Usage: /" + label + " reload");
+            return true;
+        }
+
+        if (!sender.hasPermission(RELOAD_PERMISSION)) {
+            sender.sendMessage("You do not have permission to use this command.");
+            return true;
+        }
+
+        final PluginSettings previousSettings = loadedSettings;
+        reloadConfig();
+        final PluginSettings newSettings = readSettings(getConfig());
+        final int changedRecipeCount = reconcileRecipes(previousSettings, newSettings);
+        loadedSettings = newSettings;
+
+        sender.sendMessage("RottenFlesh2Leather config reloaded. Updated recipes: " + changedRecipeCount + ".");
+        return true;
+    }
+
+    private void initializeBstats() {
+        final FileConfiguration config = getConfig();
+        final boolean bstatsEnabled = config.getBoolean("bstats-enabled", true);
+        if (!bstatsEnabled) {
+            return;
+        }
+
+        new Metrics(this, BSTATS_PLUGIN_ID);
+    }
+
+    private PluginSettings readSettings(FileConfiguration config) {
+        return new PluginSettings(
+                config.getInt("cooking-time"),
+                (float) config.getDouble("experience-gain"),
+                config.getBoolean("furnace-recipe"),
+                config.getBoolean("smoker-recipe"),
+                config.getBoolean("blasting-recipe"),
+                config.getBoolean("crafting-recipe"),
+                config.getString("crafting-recipe-top", "XXX"),
+                config.getString("crafting-recipe-mid", "XXX"),
+                config.getString("crafting-recipe-bot", "XXX")
         );
+    }
 
-        SmokingRecipe rottenLeatherSmokerRecipe = new SmokingRecipe(
-                RottenLeatherSmokerKey,
-                RottenLeather,
-                RottenFlesh,
-                exp,
-                cookingTime/2
-        );
+    private int reconcileRecipes(PluginSettings oldSettings, PluginSettings newSettings) {
+        int changedRecipeCount = 0;
 
-        ShapedRecipe rottenLeatherCraftingRecipe = new ShapedRecipe(
-                RottenLeatherCraftingKey, RottenLeather);
+        final ItemStack rottenLeather = new ItemStack(Material.LEATHER, 1);
+        final Material rottenFlesh = Material.ROTTEN_FLESH;
 
-
-        rottenLeatherCraftingRecipe.shape
-                (craftingRecipeTop, craftingRecipeMid, craftingRecipeBot);
-        rottenLeatherCraftingRecipe.setIngredient('X', Material.ROTTEN_FLESH);
-
-        if (furnaceRecipe) {
-            Bukkit.addRecipe(rottenLeatherFurnaceRecipe);
-
+        if (didFurnaceChange(oldSettings, newSettings)) {
+            Bukkit.removeRecipe(rottenLeatherFurnaceKey);
+            if (newSettings.furnaceRecipeEnabled) {
+                final FurnaceRecipe rottenLeatherFurnaceRecipe = new FurnaceRecipe(
+                        rottenLeatherFurnaceKey,
+                        rottenLeather,
+                        rottenFlesh,
+                        newSettings.exp,
+                        newSettings.cookingTime
+                );
+                Bukkit.addRecipe(rottenLeatherFurnaceRecipe);
+            }
+            changedRecipeCount++;
         }
 
-        if (craftingRecipe) {
-            Bukkit.addRecipe(rottenLeatherCraftingRecipe);
+        if (didCraftingChange(oldSettings, newSettings)) {
+            Bukkit.removeRecipe(rottenLeatherCraftingKey);
+            if (newSettings.craftingRecipeEnabled) {
+                final ShapedRecipe rottenLeatherCraftingRecipe = new ShapedRecipe(rottenLeatherCraftingKey, rottenLeather);
+                rottenLeatherCraftingRecipe.shape(
+                        newSettings.craftingRecipeTop,
+                        newSettings.craftingRecipeMid,
+                        newSettings.craftingRecipeBot
+                );
+                rottenLeatherCraftingRecipe.setIngredient('X', rottenFlesh);
+                Bukkit.addRecipe(rottenLeatherCraftingRecipe);
+            }
+            changedRecipeCount++;
         }
 
-        if (blastingRecipe) {
-            Bukkit.addRecipe(rottenLeatherBlastingRecipe);
+        if (didBlastingChange(oldSettings, newSettings)) {
+            Bukkit.removeRecipe(rottenLeatherBlastingKey);
+            if (newSettings.blastingRecipeEnabled) {
+                final BlastingRecipe rottenLeatherBlastingRecipe = new BlastingRecipe(
+                        rottenLeatherBlastingKey,
+                        rottenLeather,
+                        rottenFlesh,
+                        newSettings.exp,
+                        newSettings.cookingTime / 2
+                );
+                Bukkit.addRecipe(rottenLeatherBlastingRecipe);
+            }
+            changedRecipeCount++;
         }
 
-        if (smokerRecipe) {
-            Bukkit.addRecipe(rottenLeatherSmokerRecipe);
+        if (didSmokerChange(oldSettings, newSettings)) {
+            Bukkit.removeRecipe(rottenLeatherSmokerKey);
+            if (newSettings.smokerRecipeEnabled) {
+                final SmokingRecipe rottenLeatherSmokerRecipe = new SmokingRecipe(
+                        rottenLeatherSmokerKey,
+                        rottenLeather,
+                        rottenFlesh,
+                        newSettings.exp,
+                        newSettings.cookingTime / 2
+                );
+                Bukkit.addRecipe(rottenLeatherSmokerRecipe);
+            }
+            changedRecipeCount++;
+        }
+
+        return changedRecipeCount;
+    }
+
+    private boolean didFurnaceChange(PluginSettings oldSettings, PluginSettings newSettings) {
+        if (oldSettings == null) {
+            return newSettings.furnaceRecipeEnabled;
+        }
+        return oldSettings.furnaceRecipeEnabled != newSettings.furnaceRecipeEnabled
+                || oldSettings.cookingTime != newSettings.cookingTime
+                || Float.compare(oldSettings.exp, newSettings.exp) != 0;
+    }
+
+    private boolean didCraftingChange(PluginSettings oldSettings, PluginSettings newSettings) {
+        if (oldSettings == null) {
+            return newSettings.craftingRecipeEnabled;
+        }
+        return oldSettings.craftingRecipeEnabled != newSettings.craftingRecipeEnabled
+                || !oldSettings.craftingRecipeTop.equals(newSettings.craftingRecipeTop)
+                || !oldSettings.craftingRecipeMid.equals(newSettings.craftingRecipeMid)
+                || !oldSettings.craftingRecipeBot.equals(newSettings.craftingRecipeBot);
+    }
+
+    private boolean didBlastingChange(PluginSettings oldSettings, PluginSettings newSettings) {
+        if (oldSettings == null) {
+            return newSettings.blastingRecipeEnabled;
+        }
+        return oldSettings.blastingRecipeEnabled != newSettings.blastingRecipeEnabled
+                || oldSettings.cookingTime != newSettings.cookingTime
+                || Float.compare(oldSettings.exp, newSettings.exp) != 0;
+    }
+
+    private boolean didSmokerChange(PluginSettings oldSettings, PluginSettings newSettings) {
+        if (oldSettings == null) {
+            return newSettings.smokerRecipeEnabled;
+        }
+        return oldSettings.smokerRecipeEnabled != newSettings.smokerRecipeEnabled
+                || oldSettings.cookingTime != newSettings.cookingTime
+                || Float.compare(oldSettings.exp, newSettings.exp) != 0;
+    }
+
+    private static final class PluginSettings {
+        private final int cookingTime;
+        private final float exp;
+        private final boolean furnaceRecipeEnabled;
+        private final boolean smokerRecipeEnabled;
+        private final boolean blastingRecipeEnabled;
+        private final boolean craftingRecipeEnabled;
+        private final String craftingRecipeTop;
+        private final String craftingRecipeMid;
+        private final String craftingRecipeBot;
+
+        private PluginSettings(
+                int cookingTime,
+                float exp,
+                boolean furnaceRecipeEnabled,
+                boolean smokerRecipeEnabled,
+                boolean blastingRecipeEnabled,
+                boolean craftingRecipeEnabled,
+                String craftingRecipeTop,
+                String craftingRecipeMid,
+                String craftingRecipeBot
+        ) {
+            this.cookingTime = cookingTime;
+            this.exp = exp;
+            this.furnaceRecipeEnabled = furnaceRecipeEnabled;
+            this.smokerRecipeEnabled = smokerRecipeEnabled;
+            this.blastingRecipeEnabled = blastingRecipeEnabled;
+            this.craftingRecipeEnabled = craftingRecipeEnabled;
+            this.craftingRecipeTop = craftingRecipeTop;
+            this.craftingRecipeMid = craftingRecipeMid;
+            this.craftingRecipeBot = craftingRecipeBot;
         }
     }
 
